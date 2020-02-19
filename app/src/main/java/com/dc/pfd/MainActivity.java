d06f9e629675dcef5d4661521279b950e4e0e3da
Deepak Choudhary <choudharydeepak990@gmail.com>
2020-02-19 20:57:35 +0530

    Update MainActivity.java

--- app/src/main/java/com/dc/pfd/MainActivity.java
+++ app/src/main/java/com/dc/pfd/MainActivity.java
@@ -3,17 +3,26 @@ package com.dc.pfd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    MarksCalculator marksCalculator = new MarksCalculator(findViewById(R.id.first_insem),findViewById(R.id.second_insem),findViewById(R.id.final_sem), findViewById(R.id.attendance),findViewById(R.id.assignment))
+        float firstInSemMarks = Float.parseFloat(((EditText) findViewById(R.id.first_insem)).getText().toString());
+        float secondInSemMarks = Float.parseFloat(((EditText) findViewById(R.id.second_insem)).getText().toString());
+        float finalSemMarks = Float.parseFloat(((EditText) findViewById(R.id.final_sem)).getText().toString());
+        float assignmentMarks = Float.parseFloat(((EditText) findViewById(R.id.assignment)).getText().toString());
+        float attendanceMarks = Float.parseFloat(((EditText) findViewById(R.id.attendance)).getText().toString());
+
+        MarksCalculator marksCalculator = new MarksCalculator(firstInSemMarks,secondInSemMarks,finalSemMarks,assignmentMarks,attendanceMarks);
+    }
 
 }
