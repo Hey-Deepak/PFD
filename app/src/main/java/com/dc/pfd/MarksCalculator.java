3b9ee3c21078dec524703f415cf77672b7920d99
Deepak Choudhary <choudharydeepak990@gmail.com>
2020-02-22 21:54:48 +0530

    final1

--- app/src/main/java/com/dc/pfd/MarksCalculator.java
+++ app/src/main/java/com/dc/pfd/MarksCalculator.java
@@ -4,19 +4,32 @@ package com.dc.pfd;
 public class MarksCalculator {
 
 
-     public static String calculate(float myFirstInSem, float mySecondInSem, float myFinalSem, float myAttendance, float myAssignment) {
+    public static String calculate(float myFirstInSem, float mySecondInSem, float myFinalSem, float myAttendance, float myAssignment) {
 
         float total = ((myFirstInSem) * 30) / 100 + (mySecondInSem * 30) / 100 + (myFinalSem * 50) / 100 + (myAssignment)
                 + (myAttendance);
 
 
         if (total >= 35) {
-            return "You are Safe and you crossed Danger Zone, Your extra marks is " + (total - 35);
-        } else
-            return "You are UnSafe and you have to gain " + (35 - total) * 2
-                    + "more marks.\n Means Next time in Final Back Paper you have to score "
-                    + (myFinalSem + (35 - total) * 2);
+            if (myFinalSem >= 35) {
+                return  ("You are Safe and you crossed Danger Zone, Your extra marks is " + (total - 35) + ".");
 
+
+            } else {
+                return  ("You are UnSafe and you have to gain " + (35 - myFinalSem) + " more marks from Teacher (Current Time)." +
+                        "Means Next time in Final Back Paper you have to score Only 35 Marks. ");
+
+            }
+        } else {
+            if ((myFinalSem + (35 - total) * 2) < 35) {
+                return ((("You are UnSafe and you have to gain ") + ((35 - total) * 2) + " more marks from Teacher (Current Time)."
+                + "Means Next time in Final Back Paper you have to score Only 35 Marks."));
+
+            } else {
+                return (" You are UnSafe and you have to gain " + ((35 - total) * 2) + " more marks from Teacher (Current Time).") +
+                        ("Means Next time in Final Back Paper you have to score Only " + (myFinalSem + (35 - total) * 2) + " Marks");
+
+            }
+        }
     }
-
 }
